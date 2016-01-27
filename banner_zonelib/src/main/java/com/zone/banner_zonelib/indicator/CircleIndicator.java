33408b2f55bde06691b5b31fe871c33c3436302a
1149324777@qq.com <1149324777@qq.com>
2016-01-27 23:46:38 +0800

    indicator的设计

--- banner_zonelib/src/main/java/com/zone/banner_zonelib/indicator/CircleIndicator.java
+++ banner_zonelib/src/main/java/com/zone/banner_zonelib/indicator/CircleIndicator.java
@@ -6,9 +6,9 @@ import android.view.View;
 /**
  * Created by Administrator on 2016/1/27.
  */
-public class CircleIndicator implements IndicatorView.OnCustomViewListener {
-    @Override
-    public View getView(Context context, float percent, boolean isChecked) {
-        return null;
-    }
-}
+//public class CircleIndicator implements IndicatorView.OnCustomViewListener {
+//    @Override
+//    public View getView(Context context, float percent, boolean isChecked) {
+//        return null;
+//    }
+//}
