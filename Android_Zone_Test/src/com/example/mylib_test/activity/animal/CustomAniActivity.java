02ea42803e3333246d9a4104e13fc1ea5be442a3
fuzhipeng <1149324777@qq.com>
2016-09-14 11:34:17 +0800

    摄像机研究

--- Android_Zone_Test/src/com/example/mylib_test/activity/animal/CustomAniActivity.java
+++ Android_Zone_Test/src/com/example/mylib_test/activity/animal/CustomAniActivity.java
@@ -1,26 +1,69 @@
 package com.example.mylib_test.activity.animal;
 
-import com.example.mylib_test.R;
-import com.example.mylib_test.activity.animal.viewa.CustomAnim;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class CustomAniActivity extends Activity{
+import com.example.mylib_test.R;
+import com.example.mylib_test.activity.animal.viewa.CustomAnim;
 
-	private ImageView iv;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.a_animal_custom_ani);
-		
-		
-		iv=(ImageView) findViewById(R.id.iv);
-		CustomAnim ani = new CustomAnim();
-		ani.setRotateX(-30);
-		iv.startAnimation(ani);
-	}
+public class CustomAniActivity extends Activity {
+
+    @Bind(R.id.x)
+    Button x;
+    @Bind(R.id.y)
+    Button y;
+    @Bind(R.id.z)
+    Button z_;
+    @Bind(R.id.z_)
+    Button z;
+    @Bind(R.id.zR)
+    Button zR;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_animal_custom_ani);
+        ButterKnife.bind(this);
+
+
+        iv = (ImageView) findViewById(R.id.iv);
+
+    }
+
+    @OnClick({R.id.x, R.id.y, R.id.z, R.id.z_, R.id.zR})
+    public void onClick(View view) {
+        CustomAnim ani = new CustomAnim();
+//        ani.setRotateX(-30);
+
+        switch (view.getId()) {
+            case R.id.x:
+                ani.setX(100);
+                iv.startAnimation(ani);
+                break;
+            case R.id.y:
+                ani.setY(500);
+                iv.startAnimation(ani);
+                break;
+            case R.id.z:
+                ani.setZ(500);
+                iv.startAnimation(ani);
+                break;
+            case R.id.z_:
+                ani.setZ(-500);
+                iv.startAnimation(ani);
+                break;
+            case R.id.zR:
+                iv.animate().rotation(30).start();
+                break;
+        }
+    }
 }
--- Android_Zone_Test/src/com/example/mylib_test/activity/animal/viewa/CustomAnim.java
+++ Android_Zone_Test/src/com/example/mylib_test/activity/animal/viewa/CustomAnim.java
@@ -12,6 +12,7 @@ public class CustomAnim extends Animation {
     private int mCenterHeight;
     private Camera mCamera = new Camera();
     private float mRotateY = 0.0f;
+    private float mX = 0.0f, mY = 0.0f, mZ = 0.0f;
 
     @Override
     public void initialize(int width,int height, int parentWidth, int parentHeight) {
@@ -22,7 +23,7 @@ public class CustomAnim extends Animation {
         // 动画结束后保留状态
         setFillAfter(true);
         // 设置默认插值器
-        setInterpolator(new BounceInterpolator());
+//        setInterpolator(new BounceInterpolator());
         mCenterWidth = width / 2;
         mCenterHeight = height / 2;
     }
@@ -31,6 +32,18 @@ public class CustomAnim extends Animation {
     public void setRotateX(float rotateY) {
         mRotateY = rotateY;
     }
+    // 暴露接口-设置旋转角度
+    public void setX(float x) {
+        mX = x;
+    }
+    // 暴露接口-设置旋转角度
+    public void setY(float y) {
+        mY = y;
+    }
+    // 暴露接口-设置旋转角度
+    public void setZ(float z) {
+        mZ = z;
+    }
 
     @Override
     protected void applyTransformation(
@@ -38,11 +51,14 @@ public class CustomAnim extends Animation {
             Transformation t) {
         final Matrix matrix = t.getMatrix();
         mCamera.save();
+        mCamera.translate(mX*interpolatedTime,mY*interpolatedTime,mZ*interpolatedTime);
+//        mCamera.translate(0,0,100*interpolatedTime);
         // 使用Camera设置旋转的角度
-        mCamera.rotateX(mRotateY * interpolatedTime);
+//        mCamera.rotateX(mRotateY * interpolatedTime);
         // 将旋转变换作用到matrix上
         mCamera.getMatrix(matrix);
         mCamera.restore();
+        System.out.println(matrix.toString());
         // 通过pre方法设置矩阵作用前的偏移量来改变旋转中心
 //        matrix.preTranslate(mCenterWidth, mCenterHeight);
 //        matrix.postTranslate(-mCenterWidth, -mCenterHeight);
