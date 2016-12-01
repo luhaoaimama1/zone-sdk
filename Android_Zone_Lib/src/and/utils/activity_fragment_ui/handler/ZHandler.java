823a838b8355f07db265e31717276b2cc8d14f2d
fuzhipeng <1149324777@qq.com>
2016-12-01 11:59:18 +0800

    Zhandler  防止内存泄漏 更改

--- Android_Zone_Lib/src/and/utils/activity_fragment_ui/handler/ZHandler.java
+++ Android_Zone_Lib/src/and/utils/activity_fragment_ui/handler/ZHandler.java
@@ -1,17 +1,19 @@
 package and.utils.activity_fragment_ui.handler;
 
+import android.app.Activity;
 import android.os.Handler;
 import android.os.Message;
+
 import java.lang.ref.WeakReference;
 
 /**
  * 主要是防止空缓存
  */
-public  class ZHandler extends Handler {
-    private final WeakReference<ZCallback> mCallback;
+public class ZHandler extends Handler {
+    private final WeakReference<Activity> mCallback;
 
-    public ZHandler(ZCallback callback) {
-        mCallback = new WeakReference<ZCallback>(callback);
+    public ZHandler(Activity activity) {
+        mCallback = new WeakReference<Activity>(activity);
     }
 
     @Override
@@ -19,13 +21,17 @@ public  class ZHandler extends Handler {
         if (mCallback.get() == null) {
             return;
         }
-        mCallback.get().handleMessage(msg);
-    }
-    public interface ZCallback{
-         void handleMessage(Message msg);
+        handleMessageReplace(msg);
     }
 
-    public void removeAllMessage(){
+    public void handleMessageReplace(Message msg) {
+
+    }
+
+    /**
+     *  Tips：在生命周期结束的时候调用
+     */
+    public void removeAllMessage() {
         removeCallbacksAndMessages(null);//remove全部任务！
     }
 }
