a58dcc99ecde6d216679b92e8481dfc87deb883e
fuzhipeng <1149324777@qq.com>
2017-05-04 15:38:17 +0800

    readMe补充

--- Android_Zone_Lib/src/and/utils/zeventbus/ZEventBus.java
+++ Android_Zone_Lib/src/and/utils/zeventbus/ZEventBus.java
@@ -107,7 +107,7 @@ public class ZEventBus {
                     e("对象已被回收！");
                     return;
                 }
-                e("执行方法:"+method.getName());
+                e(String.format("执行方法:%s(%s)",method.getName(),eventObj.getClass().getSimpleName()));
                 method.invoke(callBackObj, eventObj);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
