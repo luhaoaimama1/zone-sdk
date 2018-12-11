c01bd525f3902380ef17188b1e58f7e5ae299b4e
fuzhipeng <1149324777@qq.com>
2018-12-11 18:35:46 +0800

    版本升级完毕  v4冲突 和 gradlew命令修复

--- Android_Zone_Test/src/com/example/mylib_test/delegates/FrescoSuperscriptDeletates.kt
+++ Android_Zone_Test/src/com/example/mylib_test/delegates/FrescoSuperscriptDeletates.kt
@@ -14,16 +14,18 @@ import com.zone.lib.utils.data.convert.DensityUtils
  *[2018/7/10] by Zone
  */
 
-class FrescoSuperscriptDeletates : ViewDelegates<FrescoActivity.Entity>(){
+class FrescoSuperscriptDeletates : ViewDelegates<FrescoActivity.Entity>() {
     override fun getLayoutId(): Int = R.layout.a_fresco_item
 
     override fun fillData(posi: Int, entity: FrescoActivity.Entity?, holder: Holder<out Holder<*>>?) {
         holder!!.setText(R.id.tv, entity?.introduce);
-        val sdv =holder.getView(R.id.sdv) as SimpleDraweeView;
+        val sdv = holder.getView(R.id.sdv) as SimpleDraweeView;
         val uri = Uri.parse(entity?.uri);
         sdv.setImageURI(uri);
         val width = DensityUtils.dp2px(context, 20f)
-        val drawable = SuperscriptDrawable(ContextCompat.getDrawable(context,R.drawable.icon));
+        val drawable1 = ContextCompat.getDrawable(context, R.drawable.icon)
+        if (drawable1 == null) return
+        val drawable = SuperscriptDrawable(drawable1);
         sdv.getHierarchy().setOverlayImage(drawable);
     }
 
