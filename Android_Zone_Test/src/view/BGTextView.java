7eda6f9b5e3dc5a719381f346f15fa75c5c86ab5
Zone <1149324777@qq.com>
2020-08-06 23:41:22 +0800

    更新

---
+++ Android_Zone_Test/src/view/BGTextView.java
@@ -0,0 +1,57 @@
+package view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.Layout;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+
+import com.zone.lib.utils.view.DrawUtils;
+
+public class BGTextView extends androidx.appcompat.widget.AppCompatEditText {
+    public BGTextView(Context context) {
+        super(context);
+    }
+
+    public BGTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BGTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    Paint paint2 = DrawUtils.getStrokePaint(Paint.Style.STROKE, 3);
+    int[] colorArry = new int[]{Color.CYAN, Color.GREEN, Color.YELLOW};
+    Rect rect = new Rect();
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), (int) (getMeasuredHeight() + getLineSpacingExtra()));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        float lineSpacingExtra = getLineSpacingExtra();
+        try {
+            Layout layout = getLayout();
+            int lineCount = layout.getLineCount();
+            for (int i = 0; i < lineCount; i++) {
+                paint2.setColor(colorArry[i % colorArry.length]);
+                layout.getLineBounds(i, rect);
+                if (i == lineCount - 1) {
+                    rect.bottom += lineSpacingExtra;
+                }
+                canvas.drawRect(rect, paint2);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
